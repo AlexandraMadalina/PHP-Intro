136fc779f5259f36969722445057efcdc06674d7
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-05-20 14:01:05 +0200

    img

---
+++ 0.Working-Files/Lottery/lottery.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <form action="lottery.php">
+    <input type="radio" name="pizza">
+    <input type="radio" name="pizza">
+    <input type="radio" name="pizza">
+    </form>
+</body>
+</html>
