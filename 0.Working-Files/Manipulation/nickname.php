09fce8ebb3d7b1401b0e98ebf0c615e4cd6228f1
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-05-21 12:27:53 +0200

    enable button

---
+++ 0.Working-Files/Manipulation/nickname.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <p class="d-block w-50 p-2 bg-info text-white text-center mx-auto">Cool NickName Generator</p>
+    <form action="" class="w-25 p-3 mx-auto">
+        <div class="form-group text-center">
+            <label for="nickname">Nickname</label>
+            <input type="text" class="form-control" id="nickname" placeholder="...">
+            <button type="submit" class="btn btn-info mt-4" disabled>Info</button>
+        </div>
+    </form>
+
+    <script type="text/javascript">
+        const input = document.getElementById('nickname');
+        const button = document.querySelector("button");
+
+        input.oninput = function (e) {
+            let letters = e.target.value;
+            if (letters.length > 1) {
+                button.removeAttribute("disabled");
+            }
+           
+        }
+</script>
+</body>
+
+</html>
