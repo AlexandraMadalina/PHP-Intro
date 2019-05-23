7ec4fa3f4b71cb8af6f5161ebdd7d438a5802b53
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-05-23 15:50:51 +0200

    print this final object

--- 0.Working-Files/Manipulation/Object_Array manipulation/home.php
+++ 0.Working-Files/Manipulation/Object_Array manipulation/home.php
@@ -1,10 +1,10 @@
 <?php
 
-$array = array("John", "Snow",30,);
+$array = array("John", "Snow",30);
 $associative_array = array(
-"first_name"=>"John",
-"last_name"=>"Snow",
-"age"=> 30
+"first"=>"Arya",
+"last"=>"Stark",
+"years"=> 17
 );
 class Person{
    public $first_name;
@@ -18,63 +18,51 @@ class Person{
 
 }
 $object = new Person ("John", "Snow", "30");
-var_dump($array);
-var_dump($associative_array);
-var_dump($object);
 $array1 = json_decode(json_encode($array));
-var_dump($array1);
 $all = [$array,$associative_array, $object];
-var_dump($all);
 $added_by_loop = "I am added by loop";
 foreach($all as $item){
     if(is_assoc($item)){
         $item['added'] = $added_by_loop;
-        var_dump($item);
         $keys = array_keys($item);
         $rand_no = rand(0,count( $keys)*5);
         if($rand_no<count($keys)){
-          echo "<span style=\"color:red\">The random key \"{$keys[$rand_no]}\" holds the value \"{$item[$keys[$rand_no]]}\"</span>";
+          // echo "<span style=\"color:red\">The random key \"{$keys[$rand_no]}\" holds the value \"{$item[$keys[$rand_no]]}\"</span>";
         }else{
-          echo "<span style=\"color:red\">Random key does not exist</span>";
+          // echo "<span style=\"color:red\">Random key does not exist</span>";
         }
        $last = end($item);
        $key = key($item);
        unset($item[$key]);
-          echo "<br/><br/><span style=\"color:red\">Associative array after removing the last element:</span>";
-        var_dump($item);
-        
-      }else if(is_object($item)){
+          // echo "<br/><br/><span style=\"color:red\">Associative array after removing the last element:</span>";
+      reset($item);
+        }else if(is_object($item)){
       $item->added = $added_by_loop;
-      var_dump($item);
       $array_from_object = (array)$item;
-      var_dump($array_from_object);
       $obj_keys = array_keys($array_from_object);
       $rand_no_2 = rand(0,count($obj_keys)*5);
       if($rand_no_2<count($obj_keys)){
-        echo "<span style=\"color:red\">The random key \"{$obj_keys[$rand_no_2]}\" holds the value \"{$array_from_object[$keys[$rand_no_2]]}\"</span>";
+        // echo "<span style=\"color:red\">The random key \"{$obj_keys[$rand_no_2]}\" holds the value \"{$array_from_object[$keys[$rand_no_2]]}\"</span>";
       }else{
-        echo "<span style=\"color:red\">Random key does not exist</span>";
+        // echo "<span style=\"color:red\">Random key does not exist</span>";
       }
      
     }else{
       array_push($item, $added_by_loop);
-      var_dump($item);
       $rand_no_3 = rand(0,count($item)*5);
       if($rand_no_3<count($item)){
-        echo "<span style=\"color:red\">The random index \"{[$rand_no_3]}\" holds the value \"{$item[$rand_no_3]}\"</span>";
+        // echo "<span style=\"color:red\">The random index \"{[$rand_no_3]}\" holds the value \"{$item[$rand_no_3]}\"</span>";
       }else{
-        echo "<span style=\"color:red\">Random index does not exist</span>";
+        // echo "<span style=\"color:red\">Random index does not exist</span>";
       }
       if(count($item)>2 && count($item)%2==1){
-
         $half_of_array = array_slice($item,0,(count($item)-1)/2);
       }else if(count($item)>1){
         $half_of_array = array_slice($item,0,count($item)/2);
        
       }
-      echo "<br/><br/><span style=\"color:red\">This is the sliced array:</span>";
-      var_dump($half_of_array);
-      
+      // echo "<br/><br/><span style=\"color:red\">This is the sliced array:</span>";
+         
     }
 }
  
@@ -83,4 +71,34 @@ function is_assoc($arr){
   return;
     return array_keys($arr) !== range(0, count($arr) - 1);
 }
+
+$object->arr1 = $array;
+$object->arr2 = $associative_array;
+foreach($associative_array as $key=>$value){
+  $object->$key = $value;
+  //echo "{$key} = {$value}";
+}
+$cookie = json_encode($object);
+setcookie("my_coockie",$cookie);
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Home</title>
+</head>
+<body class="p-3 mb-2 bg-dark">
+<div class="container bg-light rounded shadow py-5 px-5 centered">
+  <div class="row">
+    <div class="col">
+    <pre class="mx-auto w-50"><?php print_r(json_decode($_COOKIE['my_coockie']));?></pre>
+    </div>
+  </div>
+ </div>
+</body>
+</html>
