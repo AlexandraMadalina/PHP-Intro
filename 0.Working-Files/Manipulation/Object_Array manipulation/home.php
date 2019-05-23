f99634c958ea0b87f9ca573f3cbcc92adc66536b
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-05-23 10:56:13 +0200

    Write an if-statement that has a 20% chance to edit a random item of one of the above. Put this if statement in a loop so every array/object has a random chance of having a random item changed

--- 0.Working-Files/Manipulation/Object_Array manipulation/home.php
+++ 0.Working-Files/Manipulation/Object_Array manipulation/home.php
@@ -7,9 +7,9 @@ $associative_array = array(
 "age"=> 30
 );
 class Person{
-   private $first_name;
-   private $last_name;
-   private $age;
+   public $first_name;
+   public $last_name;
+   public $age;
    public function __construct($first, $second, $years){
     $this->first_name = $first;
     $this->last_name = $second;
@@ -30,12 +30,36 @@ foreach($all as $item){
     if(is_assoc($item)){
         $item['added'] = $added_by_loop;
         var_dump($item);
-    }else if(is_object($item)){
+        $keys = array_keys($item);
+        $rand_no = rand(0,count( $keys)*5);
+        if($rand_no<count($keys)){
+          echo "<span style=\"color:red\">The random key \"{$keys[$rand_no]}\" holds the value \"{$item[$keys[$rand_no]]}\"</span>";
+        }else{
+          echo "<span style=\"color:red\">Random key does not exist</span>";
+        }
+        
+      }else if(is_object($item)){
       $item->added = $added_by_loop;
       var_dump($item);
+      $array_from_object = (array)$item;
+      var_dump($array_from_object);
+      $obj_keys = array_keys($array_from_object);
+      $rand_no_2 = rand(0,count($obj_keys)*5);
+      if($rand_no_2<count($obj_keys)){
+        echo "<span style=\"color:red\">The random key \"{$obj_keys[$rand_no_2]}\" holds the value \"{$array_from_object[$keys[$rand_no_2]]}\"</span>";
+      }else{
+        echo "<span style=\"color:red\">Random key does not exist</span>";
+      }
+     
     }else{
       array_push($item, $added_by_loop);
       var_dump($item);
+      $rand_no_3 = rand(0,count($item)*5);
+      if($rand_no_3<count($item)){
+        echo "<span style=\"color:red\">The random index \"{[$rand_no_3]}\" holds the value \"{$item[$rand_no_3]}\"</span>";
+      }else{
+        echo "<span style=\"color:red\">Random index does not exist</span>";
+      }
     }
 }
  
